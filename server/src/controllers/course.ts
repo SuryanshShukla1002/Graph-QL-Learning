14c38093104252454868ce7770cc03788117aa1c
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-25 09:34:38 +0530

    getAllLectures async function created

---
+++ server/src/controllers/course.ts
@@ -0,0 +1,19 @@
+import { Course } from "../models/courseModel.js";
+import { Lecture } from "../models/lecture.model.js";
+
+export const getAllCourses = async () => {
+  const courses = await Course.find();
+
+  return courses;
+};
+
+export const getAllCoursesByID = async (parent: any, arg: { id: string }) => {
+  const course = await Course.findById(arg.id).populate("instructor");
+  return course;
+}; 
+
+export const getAllLectures = async () => {
+  const lectures = await Lecture.find();
+  return lectures;
+}; 
+
