a737bf4db496ac29016dbc4edc6f3f3dad08eac0
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-31 20:04:34 +0530

    get specified user

--- server/src/controllers/course.ts
+++ server/src/controllers/course.ts
@@ -10,10 +10,16 @@ export const getAllCourses = async () => {
 export const getAllCoursesByID = async (parent: any, arg: { id: string }) => {
   const course = await Course.findById(arg.id).populate("instructor");
   return course;
-}; 
+};
 
 export const getAllLectures = async () => {
   const lectures = await Lecture.find();
   return lectures;
-}; 
+};
 
+export const getCoursesOfUser = async (user: any) => {
+  const courses = await Course.find({
+    instructor: user._id,
+  });
+  return courses;
+};
