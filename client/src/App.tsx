91011317cf5079a04869ffd5f8be7b992cc146a4
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-31 20:03:43 +0530

    submit handler on form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,55 @@
-import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client"
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 import { addUser, getUsers } from "./graphql/query/query";
+import { FormEvent, useState } from "react";
 
 const App = () => {
-const [ viewUsers, {loading, data , error }] = useLazyQuery(gql(getUsers));
+  const [viewUsers, { loading, data, error }] = useLazyQuery(gql(getUsers));
+  const [add, { data: addUserResponse }] = useMutation(gql(addUser));
+  const [name, setName] = useState("");
+  const [Age, setAge] = useState("");
+  const [gender, setGender] = useState("");
 
-const [add, {data: addUserResponse} ] = useMutation(gql(addUser))
-console.log(addUserResponse);
+  console.log(addUserResponse);
+  const submitHandler = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    add({variables:{
+      name,
+      Age,
+      gender
+    }})
+  }
 
-
-if(error) return <h1>Some Error</h1>
-console.log(data);
-return loading ? (
-  <h1>Loading.....</h1>
-) : (
+  if (error) return <h1>Some Error</h1>;
+  console.log(data);
+  return loading ? (
+    <h1>Loading.....</h1>
+  ) : (
     <div>
       <h1>WOW</h1>
       {data?.courses[0]?.title}
-      <button onClick={() => viewUsers()}>View Users</button>
-      <button onClick={() => add({variables:{}})}>Add Users</button>
+      <button onClick={() => viewUsers()}>Add Users</button>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          value={Age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        />
+      </form>
+      <button type="submit">Add users </button>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
