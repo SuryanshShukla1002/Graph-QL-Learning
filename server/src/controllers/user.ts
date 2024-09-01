0477fa37a531ca6fe19f1438c649231471375eb1
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-09-02 00:20:03 +0530

    add intructor user id to find course

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,6 +1,13 @@
+import { Course } from "../models/courseModel.js";
 import { User } from "../models/userModel.js";
 
 export const getALLUsers = async () => {
   const users = await User.find();
   return users;
+};
+
+export const getUserByID = async (id: string) => {
+  const user = await User.findById(id);
+  Course.find({instructor:user?._id})
+  return user;
 };
